90108f718c90e6d0fe15d27bd5e8ab35ef48c256
choseongchan3712 <choseongchan3712@naver.com>
2024-11-04 14:50:05 +0900

    master

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useState } from "react";
+
 const App = () => {
+  const [num, setNum] = useState(0);
+
   return (
     <div>
-      <h3>0</h3>
-      <button>+</button>
-      <button>-</button>
+      <h3>{num}</h3>
+      <button onClick={() => setNum(num + 1)}>+</button>
+      <button onClick={() => setNum(num - 1)}>+</button>
     </div>
   );
 };
